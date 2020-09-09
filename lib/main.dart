1b84d0893afcd15a8c7433b62345f39619b7bbff
João <jv.clavilho@hotmail.com>
2020-09-09 16:58:12 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nubank/pages/home/home_page.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarBrightness: Brightness.dark));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Nubank do João',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          brightness: Brightness.dark,
+        ),
+        home: HomePage());
+  }
+}
---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:nubank/pages/home/widgets/my_app_bar.dart';
+import 'package:nubank/pages/home/widgets/my_dots_app.dart';
+import 'package:nubank/pages/home/widgets/page_view_app.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _showMenu;
+  int _currentIndex;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _showMenu = false;
+    _currentIndex = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double _screenHeigth = MediaQuery.of(context).size.height;
+    return Scaffold(
+        backgroundColor: Colors.purple[800],
+        body: Stack(
+          alignment: Alignment.topCenter,
+          children: <Widget>[
+            MyAppBar(
+              showMenu: _showMenu,
+              onTap: () {
+                setState(() {
+                  _showMenu = !_showMenu;
+                });
+              },
+            ),
+            PageViewApp(
+              top: _screenHeigth * .24,
+              onChanged: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+            ),
+            Positioned(
+              top: _screenHeigth * .70,
+              child: MyDotsApp(
+                currentIndex: _currentIndex,
+              ),
+            )
+          ],
+        ));
+  }
+}
